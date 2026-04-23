aad4d20febd98206bf3c751869dcf96305148a03
KhadkaRenish <githubrenish@gmail.com>
2026-04-23 21:12:28 +0545

    Created reusable navbar and hero components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
+import Navbar from "./components/ Navbar";
+import Hero from "./components/Hero";
+
+
 function App() {
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "#0f172a",
-        color: "white",
-        textAlign: "center",
-        padding: "2rem",
-      }}
-    >
-      <div>
-        <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>
-          Hi, I’m Renish
-        </h1>
-        <p style={{ fontSize: "1.2rem", marginBottom: "1.5rem" }}>
-          I build modern web applications.
-        </p>
-        <button
-          style={{
-            padding: "12px 24px",
-            border: "none",
-            borderRadius: "10px",
-            background: "#38bdf8",
-            color: "#0f172a",
-            fontWeight: "bold",
-            cursor: "pointer",
-          }}
-        >
-          View My Projects
-        </button>
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+function Hero() {
+  return (
+    <section
+      id="home"
+      style={{
+        minHeight: "90vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#0f172a",
+        color: "white",
+        textAlign: "center",
+        padding: "2rem"
+      }}
+    >
+      <div>
+        <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>Hi, I’m Renish</h1>
+        <p style={{ fontSize: "1.2rem" }}>I build modern web applications.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
